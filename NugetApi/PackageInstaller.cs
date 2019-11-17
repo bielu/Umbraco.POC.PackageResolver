d05170ddb01dc47a984ee75b81ab4b012299fdb6
arkadiuszbiel <abi@novicell.co.uk>
2019-11-17 16:27:34 +0000

    POC package resolving with dependencies

---
+++ NugetApi/PackageInstaller.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Frameworks;
+using NuGet.Packaging;
+using NuGet.Packaging.Core;
+using NuGet.Packaging.Signing;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Resolver;
+using NuGet.Versioning;
+
+namespace NugetApi
+{
+    public class PackageInstaller
+    {
+        public static async Task InstallWithDependencies(string packageId, string version, string frameworkVersion, ILogger logger)
+        {
+
+            var packageVersion = NuGetVersion.Parse(version);
+            var nuGetFramework = NuGetFramework.ParseFolder(frameworkVersion);
+            var settings = Settings.LoadDefaultSettings(root: null);
+            var feed = new Uri("https://api.nuget.org/v3/index.json");
+            var repositoryList = new List<SourceRepository>();
+            repositoryList.Add(Repository.Factory.GetCoreV3(feed.AbsoluteUri, FeedType.HttpV3));
+            var sourceRepositoryProvider = new SourceRepositoryProvider(settings, Repository.Provider.GetCoreV3());
+            using (var cacheContext = new SourceCacheContext())
+            {
+
+                var availablePackages = new HashSet<SourcePackageDependencyInfo>(PackageIdentityComparer.Default);
+                await GetPackageDependencies(
+                    new PackageIdentity(packageId, packageVersion),
+                    nuGetFramework, cacheContext, logger, repositoryList, availablePackages);
+
+                var resolverContext = new PackageResolverContext(
+                    DependencyBehavior.Lowest,
+                    new[] {packageId},
+                    Enumerable.Empty<string>(),
+                    Enumerable.Empty<PackageReference>(),
+                    Enumerable.Empty<PackageIdentity>(),
+                    availablePackages,
+                    sourceRepositoryProvider.GetRepositories().Select(s => s.PackageSource),
+                    logger);
+
+                var resolver = new PackageResolver();
+                var packagesToInstall = resolver.Resolve(resolverContext, CancellationToken.None)
+                    .Select(p => availablePackages.Single(x => PackageIdentityComparer.Default.Equals(x, p)));
+                var packagePathResolver = new PackagePathResolver(Path.GetFullPath("packages"));
+                var packageExtractionContext = new PackageExtractionContext(
+                    PackageSaveMode.Defaultv3,
+                    XmlDocFileSaveMode.None,
+                    ClientPolicyContext.GetClientPolicy(settings, logger),
+                    logger);
+                //new PackageSignatureVerifier(
+                //  signatureVerification.GetSignatureVerificationProviders()),
+                //SignedPackageVerifierSettings.GetDefault());
+                var frameworkReducer = new FrameworkReducer();
+
+                foreach (var packageToInstall in packagesToInstall)
+                {
+                    InstallPackages(packagePathResolver, packageToInstall, cacheContext, packageExtractionContext,
+                        frameworkReducer, nuGetFramework, settings,logger);
+                }
+            }
+        }
+
+        static async Task GetPackageDependencies(PackageIdentity package,
+                NuGetFramework framework,
+                SourceCacheContext cacheContext,
+                ILogger logger,
+                IEnumerable<SourceRepository> repositories,
+                ISet<SourcePackageDependencyInfo> availablePackages)
+            {
+                if (availablePackages.Contains(package)) return;
+
+                foreach (var sourceRepository in repositories)
+                {
+                    var dependencyInfoResource = await sourceRepository.GetResourceAsync<DependencyInfoResource>();
+           
+                    var dependencyInfo = await dependencyInfoResource.ResolvePackage(
+                        package, framework, cacheContext, logger, CancellationToken.None);
+
+                    if (dependencyInfo == null) continue;
+
+                    availablePackages.Add(dependencyInfo);
+                    foreach (var dependency in dependencyInfo.Dependencies)
+                    {
+                        await GetPackageDependencies(
+                            new PackageIdentity(dependency.Id, dependency.VersionRange.MinVersion),
+                            framework, cacheContext, logger, repositories, availablePackages);
+                    }
+                }
+            }
+
+        public static async Task InstallPackages(PackagePathResolver packagePathResolver,SourcePackageDependencyInfo packageToInstall,SourceCacheContext cacheContext,
+           PackageExtractionContext packageExtractionContext,FrameworkReducer frameworkReducer,NuGetFramework nuGetFramework, ISettings settings, ILogger logger)
+        {
+             PackageReaderBase packageReader;
+                    var installedPath = packagePathResolver.GetInstalledPath(packageToInstall);
+                    if (installedPath == null)
+                    {
+                        var downloadResource =
+                            await packageToInstall.Source.GetResourceAsync<DownloadResource>(CancellationToken.None);
+                        var downloadResult = await downloadResource.GetDownloadResourceResultAsync(
+                            packageToInstall,
+                            new PackageDownloadContext(cacheContext),
+                            SettingsUtility.GetGlobalPackagesFolder(settings),
+                            logger, CancellationToken.None);
+
+                        await PackageExtractor.ExtractPackageAsync(
+                            downloadResult.PackageSource,
+                            downloadResult.PackageStream,
+                            packagePathResolver,
+                            packageExtractionContext,
+                            CancellationToken.None);
+
+                        packageReader = downloadResult.PackageReader;
+                    }
+                    else
+                    {
+                        packageReader = new PackageFolderReader(installedPath);
+                    }
+
+                    var libItems = packageReader.GetLibItems();
+                    var nearest = frameworkReducer.GetNearest(nuGetFramework, libItems.Select(x => x.TargetFramework));
+                    Console.WriteLine(string.Join("\n", libItems
+                        .Where(x => x.TargetFramework.Equals(nearest))
+                        .SelectMany(x => x.Items)));
+
+                    var frameworkItems = packageReader.GetFrameworkItems();
+                    nearest = frameworkReducer.GetNearest(nuGetFramework,
+                        frameworkItems.Select(x => x.TargetFramework));
+                    Console.WriteLine(string.Join("\n", frameworkItems
+                        .Where(x => x.TargetFramework.Equals(nearest))
+                        .SelectMany(x => x.Items)));
+        }
+    }
+}
---
+++ NugetCustomConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using NugetApi;
+using NugetApi.Loggers;
+
+namespace NugetCustomConsole
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+          await PackageInstaller.InstallWithDependencies("UmbracoCms","8.3.0","net472", new Logger());
+        }
+    }
+}
